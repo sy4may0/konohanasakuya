ecf63409575d856dc47470cc8d69ebd844602452
sy4may0 <sy4may0@gmail.com>
2020-04-01 17:08:10 +0900

    release 0.1.0

--- server.py
+++ server.py
@@ -3,19 +3,22 @@ import json
 import dotenv
 import os
 import base64
-from flask import Flask, request, jsonify
+from pymongo import MongoClient
+from flask import Flask, make_response, request, jsonify
 from flask_cors import CORS 
 
 from WeeklyReport import WeeklyReport
 
 dotenv.load_dotenv()
 
-DISTRIBUTE_REPO = os.environ.get("DISTRIBUTE_REPO")
-DISTRIBUTE_URL = os.environ.get("DISTRIBUTE_URL")
+DISTRIBUTE_REPO =  os.path.dirname(__file__) + '/public'
+DISTRIBUTE_URL = os.environ.get("URL")
 PORT = os.environ.get("PORT")
 
 app = Flask(__name__)
 CORS(app)
+
+mongoClient = MongoClient(os.environ.get('MONGO_URI'))
 
 #
 # {
@@ -27,17 +30,35 @@ CORS(app)
 def build_excel():
     data = request.json
 
+    # Get Username text.
+    db = mongoClient['oshihomimi']
+    col = db.get_collection('users')
+    userData = col.find_one(filter={'name': data['user']})
+    userNameText = userData['text']
+
     filename = base64.urlsafe_b64encode(
                     data['user'].encode('utf-8')
                ).decode()
     distribute_path = os.path.join(DISTRIBUTE_REPO, filename)
 
-    weeklyReport = WeeklyReport(data['user'], data['achievements'])
+    weeklyReport = WeeklyReport(userNameText, data['achievements'])
     weeklyReport.writeToExcel(distribute_path)
 
     download_link = os.path.join(DISTRIBUTE_URL, filename)
 
     return jsonify({ 'download_link': download_link })
+
+@app.route('/<string:filename>', methods=['GET'])
+def download_excel(filename):
+    response = make_response()
+    distribute_path = os.path.join(DISTRIBUTE_REPO, filename)
+    response.data = open(distribute_path, 'rb').read()
+
+    downloadFileName = base64.urlsafe_b64decode(filename).decode('utf-8') + '.xlsx'
+    response.headers['Content-Disposition'] = 'attachment; filename=' + downloadFileName
+
+    response.mimetype = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='127.0.0.1', port=PORT)
