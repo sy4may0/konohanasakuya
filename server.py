9ead7b491cf30b04e43c747764ed75901de0cd73
sy4may0 <sy4may0@ik1-315-17839.vs.sakura.ne.jp>
2020-04-03 17:23:32 +0900

    fix public file not exists

--- server.py
+++ server.py
@@ -20,6 +20,9 @@ PORT = os.environ.get("PORT")
 app = Flask(__name__)
 CORS(app)
 
+if os.path.isdir(DISTRIBUTE_REPO):
+    os.path.mkdir(DISTRIBUTE_REPO)
+
 mongoClient = MongoClient(os.environ.get('MONGO_URI'))
 
 #
